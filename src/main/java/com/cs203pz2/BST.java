eb2e3c1726362db6bfafdb23a3d7fd3d6b771dd0
milicaml <152500785+milicaml@users.noreply.github.com>
2024-06-04 14:37:39 +0200

    Init

--- src/main/java/com/cs203pz2/BST.java
+++ src/main/java/com/cs203pz2/BST.java
@@ -1,2 +1,103 @@
-package com.cs203pz2;public class BST {
+package com.cs203pz2;
+
+import javafx.animation.FillTransition;
+import javafx.animation.SequentialTransition;
+import javafx.animation.StrokeTransition;
+import javafx.scene.DepthTest;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
+import javafx.util.Duration;
+
+public class BST {
+    private Node root;
+
+    public BST() {
+        this.root = null;
+    }
+
+    public void insert(int data) {
+        this.root = insertRec(this.root, data);
+    }
+
+    private Node insertRec(Node root, int data) {
+        if (root == null) {
+            root = new Node(data);
+            return root;
+        }
+
+        if (data < root.data) {
+            root.left = insertRec(root.left, data);
+        } else {
+            root.right = insertRec(root.right, data);
+        }
+
+        return root;
+    }
+
+    public void drawTree(final Pane content) {
+        content.getChildren().clear();
+        drawTreeRec(content, this.root, HelloApplication.WINDOW_WIDTH * 0.5, 100, 100);
+    }
+
+    private void drawTreeRec(final Pane content, Node root, double x, double y, double hGap) {
+        if (root != null) {
+            if (root.left != null) {
+                Line line = new Line(x, y, x - hGap, y + 50);
+                content.getChildren().add(line);
+            }
+            if (root.right != null) {
+                Line line = new Line(x, y, x + hGap, y + 50);
+                content.getChildren().add(line);
+            }
+
+            Circle circle = new Circle(x, y, 20);
+            circle.setFill(Color.LIGHTBLUE);
+            content.getChildren().add(circle);
+
+            Text text = new Text(x - 5, y + 5, String.valueOf(root.data));
+            content.getChildren().add(text);
+
+            drawTreeRec(content, root.left, x - hGap, y + 50, hGap / 2);
+            drawTreeRec(content, root.right, x + hGap, y + 50, hGap / 2);
+        }
+    }
+
+    public void animatedSearch(final Pane content, int key) {
+        drawTree(content);
+        SequentialTransition sequentialTransition = new SequentialTransition();
+
+        animatedSearchRec(content, this.root, HelloApplication.WINDOW_WIDTH * 0.5, 100, 100, key, sequentialTransition);
+
+        sequentialTransition.play();
+    }
+
+    private void animatedSearchRec(final Pane content, Node root, double x, double y, double hGap, int key, final SequentialTransition seqTransition) {
+        if (root != null) {
+            Circle circle = new Circle(x, y, 20);
+            circle.setFill(Color.LIGHTBLUE);
+            content.getChildren().add(circle);
+
+            Text text = new Text(x - 5, y + 5, String.valueOf(root.data));
+            content.getChildren().add(text);
+
+            FillTransition ft = new FillTransition(Duration.millis(1000), circle);
+            if (root.data == key) {
+                ft.setToValue(Color.GREEN);
+            } else {
+                ft.setToValue(Color.RED);
+            }
+            seqTransition.getChildren().add(ft);
+
+            if (root.data > key) {
+                animatedSearchRec(content, root.left, x - hGap, y + 50, hGap / 2, key, seqTransition);
+            } else if (root.data < key) {
+                animatedSearchRec(content, root.right, x + hGap, y + 50, hGap / 2, key, seqTransition);
+            }
+        }
+    }
+
 }
--- src/main/java/com/cs203pz2/HelloApplication.java
+++ src/main/java/com/cs203pz2/HelloApplication.java
@@ -1,17 +1,69 @@
 package com.cs203pz2;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
+//The preceding exercise simply highlights a search path.
+//Write a Java applet that animates how a search is performed. First you see that the root is
+//searched, and then a subtree is searched recursively. When a node is searched, the node
+//is highlighted. The search stops when a key is found in the tree, or the applet displays a
+//message that a key is not in the tree
 public class HelloApplication extends Application {
+    public static final int WINDOW_WIDTH = 800;
+    public static final int WINDOW_HEIGHT = 400;
+
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        BorderPane root = new BorderPane();
+
+        Pane content = new Pane();
+        root.setCenter(content);
+
+        BST tree = new BST();
+
+        tree.insert(10);
+        tree.insert(5);
+        tree.insert(15);
+        tree.insert(3);
+        tree.insert(7);
+        tree.insert(12);
+        tree.insert(17);
+        tree.drawTree(content);
+
+        Button addButton = new Button("Add");
+        Label addLabel = new Label("Enter a node");
+        TextField valueField = new TextField();
+        Button searchButton = new Button("Search");
+        Label messageLabel = new Label("Enter a key");
+        TextField keyField = new TextField();
+
+        HBox hBox = new HBox();
+        hBox.getChildren().addAll(addLabel, valueField, addButton, messageLabel, keyField, searchButton);
+        hBox.setAlignment(javafx.geometry.Pos.CENTER);
+        hBox.setSpacing(15);
+        root.setBottom(hBox);
+
+        addButton.setOnAction(e -> {
+            tree.insert(Integer.parseInt(valueField.getText()));
+            tree.drawTree(content);
+        });
+        searchButton.setOnAction(e -> {
+            tree.animatedSearch(content, Integer.parseInt(keyField.getText()));
+        });
+
+        Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
